0a824d2a218896d60a230362e47c3ed53893dc9d
postzpua <67365902+postzpua@users.noreply.github.com>
2020-06-24 18:22:35 +0200

    Lesson 6_24.06.2020_Work

---
+++ app/src/main/java/space/lysenko/quiz/GameLevels.java
@@ -0,0 +1,49 @@
+package space.lysenko.quiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameLevels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gamelevels);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        Button button_back = findViewById(R.id.button_back);
+
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // команда для кнопки
+
+
+                //начало конструкции
+                try{
+
+                    Intent intent = new Intent (GameLevels.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }catch (Exception e){
+
+                }
+                //конец конструкции
+
+
+            }
+        });
+
+
+    }
+}
--- app/src/main/java/space/lysenko/quiz/MainActivity.java
+++ app/src/main/java/space/lysenko/quiz/MainActivity.java
@@ -2,9 +2,12 @@ package space.lysenko.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +16,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Window w = getWindow();
-        //w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Button buttonStart = findViewById(R.id.buttonStart);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+
+                    Intent intent = new Intent (MainActivity.this, GameLevels.class);
+                    startActivity(intent);
+                    finish();
+
+                }catch (Exception e){
+
+                } // конец конструкции
+            }
+        });
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
